f02026912e4d3a12c4c9b520009d2c25c226c5a6
Travis.P <34238377+delenko@users.noreply.github.com>
2019-12-17 00:19:24 -0700

    Update Database.java

--- src/sample/Database.java
+++ src/sample/Database.java
@@ -1,4 +1,7 @@
 package sample;
+import com.mysql.cj.protocol.Resultset;
+
+import javax.xml.transform.Result;
 import java.sql.*;
 
 public class Database {
@@ -12,7 +15,21 @@ public class Database {
             Connection conn = DriverManager.getConnection(url,user,password);
             Statement mystmt = conn.createStatement();
             String sql = "";
-            //ResultSet rs = mystmt.executeQuery(sql);
+            String checkSQL = "Select name from bugs.bugs";
+            ResultSet rs = mystmt.executeQuery(checkSQL);
+            while(rs.next()){
+                if(rs.getString("name").equals(name)){
+                    
+                    return;
+                }else{
+                    System.out.println(rs.getString("name"));
+                    rs.close();
+                    mystmt.executeUpdate("INSERT INTO bugs.bugs "+"(name,date,status,explanation)"+"values ('"+name+"','"+date+"','"+status+"','"+explain+"')");
+                }
+            }
+
+
+
             conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
