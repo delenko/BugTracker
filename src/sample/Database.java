164175f8bea84dbefe5ee0a598a9faa32586db11
Travis <cobramustang13@hotmail.com>
2020-01-18 22:51:05 -0700

    wrong

---
+++ src/sample/Database.java
@@ -0,0 +1,40 @@
+package sample;
+
+
+import javax.xml.transform.Result;
+import java.sql.*;
+
+public class Database {
+
+    public void startDatabase(String name, String date, String status, String explain){
+        String url = "jdbc:mysql://localhost:3306";
+        String user = "root";
+        String password = "1qaz!QAZ";
+        try {
+
+            Connection conn = DriverManager.getConnection(url,user,password);
+            Statement mystmt = conn.createStatement();
+            String sql = "";
+            String checkSQL = "Select name from bugs.bugs";
+            ResultSet rs = mystmt.executeQuery(checkSQL);
+            while(rs.next()){
+                if(rs.getString("name").equals(name)){
+
+                    return;
+                }else{
+                    System.out.println(rs.getString("name"));
+                    rs.close();
+                    mystmt.executeUpdate("INSERT INTO bugs.bugs "+"(name,date,status,explanation)"+"values ('"+name+"','"+date+"','"+status+"','"+explain+"')");
+                }
+            }
+
+
+
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
